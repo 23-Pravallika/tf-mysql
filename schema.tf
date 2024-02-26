8d8d1ab345932fc574af801abf8f00d612208c75
23-Pravallika <learngain23@gmail.com>
2024-02-26 10:21:50 +0530

    AutoPush

--- schema.tf
+++ schema.tf
@@ -1,15 +1,15 @@
-# resource "null_resource" "schema" {
+resource "null_resource" "schema" {
 
-#     depends_on = [aws_db_instance.mysql]
+    depends_on = [aws_db_instance.mysql]
   
-#   provisioner "local-exec" {
-#     command = <<EOF
-#     cd /tmp
-#     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-#     unzip -o mysql.zip 
-#     cd mysql-main 
-#     mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 < shipping.sql
-#     EOF
-#   }
-# }
+  provisioner "local-exec" {
+    command = <<EOF
+    cd /tmp
+    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+    unzip -o mysql.zip 
+    cd mysql-main 
+    mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 < shipping.sql
+    EOF
+  }
+}
 
